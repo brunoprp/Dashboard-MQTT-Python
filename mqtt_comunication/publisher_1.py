ebfd2d7b5881d5186c0fb6523a6a4303eb2091e0
Bruno Santos <domingosbruno4764@gmail.com>
2021-03-04 17:31:32 -0300

    teste de transport de imagens com mqtt

--- mqtt_comunication/publisher_1.py
+++ mqtt_comunication/publisher_1.py
@@ -8,15 +8,21 @@ Created on Thu Feb 18 20:57:40 2021
 import paho.mqtt.client as mqtt 
 from temperature import measureTemp # Temperatura da CPU
 import time
+from encoder_base64 import ImgEncodDecod64
+
+obj_img_base64 = ImgEncodDecod64()
 
 mqttBroker ="sistemas.amsolution.com.br"  #  Ip do mqttBroke port= 1883
 
 client = mqtt.Client("Temperatura_PC_Edvan_viado") # ID do cliente mqtt
 client.connect(mqttBroker, port=1883) # Se conectando ao Broker
 
+string_img = obj_img_base64.encoder('input.jpg', 'imagem_bin')
 while True:
     # Publicando a informação no canal TEMPERATURE
     client.publish("TEMPERATURE", "PC_Edvan_viado: "+str(measureTemp()))
+    
+    client.publish("IMAGEM_BASE64", string_img)
     print("Just published " + "PC_Edvan_viado: "+str(measureTemp()) + " to topic TEMPERATURE")
     # Publicando a cada 1 Segundo
     time.sleep(1)
--- mqtt_comunication/subscribe.py
+++ mqtt_comunication/subscribe.py
@@ -7,24 +7,39 @@ Created on Thu Feb 18 21:18:18 2021
 """
 import paho.mqtt.client as mqtt
 import time
+from encoder_base64 import ImgEncodDecod64
+
+obj_img_base64 = ImgEncodDecod64()
+data = ''
 
 def on_message(client, userdata, message):
     print("received message: " ,str(message.payload.decode("utf-8")))
+    global data 
+    data = str(message.payload.decode("utf-8"))
 
-mqttBroker ="192.168.4.62"
+mqttBroker ="sistemas.amsolution.com.br"
 
 client = mqtt.Client("Smartphone")
 client.connect(mqttBroker) 
 
-
-
-
 client.loop_start()
-client.subscribe("TEMPERATURE")
-client.on_message=on_message
-    
-   
-# Receder dados do mqttBroker no Canal temperatura por 10 Segundos
-time.sleep(10)
+client.subscribe("IMAGEM_BASE64")
+client.on_message = on_message
+
+# Receder dados do mqttBroker no Canal temperatura por 3 Segundos
+time.sleep(3)
 client.loop_stop()
+
+
+# Sanvando a imagem como .bin para decodificar
+save_file_bin = "data_imagem"
+obj_img_base64.saveStringBin(data, "save_file_bin")
+
+# Decoficando a imagem
+name_save_img = "Img_save"
+obj_img_base64.decoder("save_file_bin.bin", name_save_img)
+
+
+
+
 
